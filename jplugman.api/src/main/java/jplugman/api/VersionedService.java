fb6827298c9623ce7948a703055023950a127934
Bastien Aracil <findbastien@gmail.com>
2021-03-31 16:07:55 +0200

    Change service casting on class equality.

--- jplugman.api/src/main/java/jplugman/api/VersionedService.java
+++ jplugman.api/src/main/java/jplugman/api/VersionedService.java
@@ -12,6 +12,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class VersionedService {
 
+
+    /**
+     * The class that the service provides.
+     * This is not necessarily exactly the class of
+     * the membre <code>service</code> (but it is
+     * a super of it).
+     */
+    private final @NonNull Class<?> serviceType;
+
     /**
      * the service
      */
@@ -26,11 +35,11 @@ public class VersionedService {
     /**
      * @param clazz the class the service should be casted to
      * @param <U> the type of the class the service should be casted to
-     * @return an optional containing the casted service
-     * if it is an instance of <code>clazz</code>, an empty optional otherwise
+     * @return an optional containing the casted service if the requested
+     * class is <br>the same as</br> the one provided by this
      */
     public <U> @NonNull Optional<U> getInstanceAs(@NonNull Class<U> clazz) {
-        if (clazz.isInstance(service)) {
+        if (serviceType.equals(clazz)) {
             return Optional.of(clazz.cast(service));
         }
         return Optional.empty();
