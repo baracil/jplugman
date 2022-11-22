73c556ada7fadceeab95d06023a189660d793580
Bastien Aracil <findbastien@gmail.com>
2022-11-22 23:37:00 +0100

    add java doc to Plugin

--- jplugman.api/src/main/java/jplugman/api/Plugin.java
+++ jplugman.api/src/main/java/jplugman/api/Plugin.java
@@ -6,18 +6,26 @@ import java.util.Set;
 
 public interface Plugin {
 
-    @NonNull Class<?> getServiceClass();
+  @NonNull Class<?> getServiceClass();
 
-    /**
-     * @return load the service provided by this plugin
-     */
-    @NonNull Object loadService(@NonNull ModuleLayer pluginLayer, @NonNull ServiceProvider serviceProvider);
+  /**
+   * load and return the service provided by this plugin as an opaque object.
+   * The plugin should plug the service within this method call as no other call to it will be made
+   * expect when unplugged it. </p>
+   * <p>
+   * If the returned object implements {@link Disposable},
+   * the method {@link Disposable#dispose()} will
+   * be called after the plugin has been unplugged from the application
+   *
+   * @return load the service provided by this plugin
+   */
+  @NonNull Object loadService(@NonNull ModuleLayer pluginLayer, @NonNull ServiceProvider serviceProvider);
 
     /**
      * @return the set of services this plugin needs to load the service it provides
      */
     @NonNull Set<Requirement<?>> getRequirements();
 
-    default void unloadService() {}
+  default void unloadService() {}
 
 }
